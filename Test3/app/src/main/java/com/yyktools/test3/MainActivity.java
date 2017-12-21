1984ac78e3a35b18fa0f90fa5efabbcd92f7cbd7
Yuri Kuznetsov <yyktomtom@gmail.com>
2017-12-20 19:34:52 -0600

    Added compute function

--- Test3/app/src/main/java/com/yyktools/test3/MainActivity.java
+++ Test3/app/src/main/java/com/yyktools/test3/MainActivity.java
@@ -13,6 +13,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private double _diam; /* D */
+    private double _pitch; /* E */
+    private double _rpm;  /* F */
+    private int _nBlades; /* G */
+    private double _load, _hp, _thrust, _speed;
+
+    public final static int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,5 +66,63 @@ public class MainActivity extends AppCompatActivity {
 
         hpOutW.setText(String.format("Version:"));
         loadOutW.setText(String.format("2017/12/20.01"));
+    }
+
+    public void computeButtonPressed(View v) {
+        EditText diamW = (EditText) findViewById(R.id.editDiam); /* D */
+        EditText pitchW = (EditText) findViewById(R.id.editPitch);   /* E */
+        EditText rpmW = (EditText) findViewById(R.id.editRPM);       /* F */
+        EditText bladesW = (EditText) findViewById(R.id.editBlades); /* G */
+
+        TextView hpOutW = (TextView) findViewById(R.id.textOut1);
+        TextView loadOutW = (TextView) findViewById(R.id.textOut2);
+
+        double diam = 0; /* D */
+        double pitch = 0; /* E */
+        double rpm = 0;  /* F */
+        int nBlades = 2; /* G */
+
+        try {
+            diam = Double.parseDouble(diamW.getText().toString());
+        } catch (Exception e) { }
+        try {
+            pitch = Double.parseDouble(pitchW.getText().toString());
+        } catch (Exception e) { }
+        try {
+            rpm = Double.parseDouble(rpmW.getText().toString());
+        } catch (Exception e) { }
+        try {
+            nBlades = Integer.parseInt(bladesW.getText().toString());
+        } catch (Exception e) { }
+        bladesW.setText(Integer.toString(nBlades));
+
+        double load, hp, thrust, speed;
+        {
+            double H = -0.05 * nBlades * nBlades + 0.65 * nBlades - 0.1;
+            double I = pitch / diam;
+            double J = diam * diam * diam * diam * pitch;  // load =POWER(D36,4)*E36
+            load = J;
+            double K = pitch * 0.000947 * rpm; // speed (mph) =E36*0.000947*F36
+            speed = K;
+            // hp =(POWER(F36,3)*POWER(D36,5))/1000000000000000000*I36*7.143*G36/2
+            double L = Math.pow(rpm, 3) * Math.pow(diam, 5) / 1000000000000000000.0 * I * 7.143 * nBlades / 2;
+            hp = L;
+            // thrust(lds)  =(POWER(F36,2)*POWER(D36,4))/1000000000000*2.83*H36
+            double M = (Math.pow(rpm, 2) * Math.pow(diam, 4) / 1000000000000.0 * 2.83 * H);
+            thrust = M;
+        }
+
+        hpOutW.setText(String.format("%.3fhp, %.2flbs, %.0fmph", hp, thrust, speed));
+        loadOutW.setText(String.format("%.0f prop load", load));
+        // save input and results
+        _diam = diam;
+        _pitch = pitch;
+        _rpm = rpm;
+        _nBlades = nBlades;
+
+        _hp = hp;
+        _thrust = thrust;
+        _speed = speed;
+        _load = load;
     }
 }
