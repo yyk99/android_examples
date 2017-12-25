734b9b8a1a6885f3cf0a2e2980d581ae8b743a8c
Yuri Kuznetsov <yyktomtom@gmail.com>
2017-12-25 12:12:10 -0600

    Commented some code

--- List/app/src/main/java/com/yyktools/list/LogRecordsActivity.java
+++ List/app/src/main/java/com/yyktools/list/LogRecordsActivity.java
@@ -20,27 +20,27 @@ public class LogRecordsActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                android.content.Intent upIntent = NavUtils.getParentActivityIntent(this);
-                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-                    // This activity is NOT part of this app's task, so create a new task
-                    // when navigating up, with a synthesized back stack.
-                    TaskStackBuilder.create(this)
-                            // Add all of this activity's parents to the back stack
-                            .addNextIntentWithParentStack(upIntent)
-                            // Navigate up to the closest parent
-                            .startActivities();
-                } else {
-                    // This activity is part of this app's task, so simply
-                    // navigate up to the logical parent activity.
-                    NavUtils.navigateUpTo(this, upIntent);
-                }
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+//        switch (item.getItemId()) {
+//            // Respond to the action bar's Up/Home button
+//            case android.R.id.home:
+//                android.content.Intent upIntent = NavUtils.getParentActivityIntent(this);
+//                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+//                    // This activity is NOT part of this app's task, so create a new task
+//                    // when navigating up, with a synthesized back stack.
+//                    TaskStackBuilder.create(this)
+//                            // Add all of this activity's parents to the back stack
+//                            .addNextIntentWithParentStack(upIntent)
+//                            // Navigate up to the closest parent
+//                            .startActivities();
+//                } else {
+//                    // This activity is part of this app's task, so simply
+//                    // navigate up to the logical parent activity.
+//                    NavUtils.navigateUpTo(this, upIntent);
+//                }
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
