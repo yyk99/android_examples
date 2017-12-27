651ec0fa87823737dc9279678ca5cebed34be428
Yuri Kuznetsov <yyktomtom@gmail.com>
2017-12-27 16:01:07 -0600

    Before merge from office

--- List/app/src/main/java/com/yyktools/list/SettingsActivity.java
+++ List/app/src/main/java/com/yyktools/list/SettingsActivity.java
@@ -152,29 +152,32 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         loadHeadersFromResource(R.xml.pref_headers, target);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                android.content.Intent upIntent = NavUtils.getParentActivityIntent(this);
-                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-                    // This activity is NOT part of this app's task, so create a new task
-                    // when navigating up, with a synthesized back stack.
-                    TaskStackBuilder.create(this)
-                            // Add all of this activity's parents to the back stack
-                            .addNextIntentWithParentStack(upIntent)
-                            // Navigate up to the closest parent
-                            .startActivities();
-                } else {
-                    // This activity is part of this app's task, so simply
-                    // navigate up to the logical parent activity.
-                    NavUtils.navigateUpTo(this, upIntent);
-                }
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (super.onOptionsItemSelected(item) == true)
+//            return true;
+//
+//        switch (item.getItemId()) {
+//            // Respond to the action bar's Up/Home button
+//            case android.R.id.home:
+//                android.content.Intent upIntent = NavUtils.getParentActivityIntent(this);
+//                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+//                    // This activity is NOT part of this app's task, so create a new task
+//                    // when navigating up, with a synthesized back stack.
+//                    TaskStackBuilder.create(this)
+//                            // Add all of this activity's parents to the back stack
+//                            .addNextIntentWithParentStack(upIntent)
+//                            // Navigate up to the closest parent
+//                            .startActivities();
+//                } else {
+//                    // This activity is part of this app's task, so simply
+//                    // navigate up to the logical parent activity.
+//                    NavUtils.navigateUpTo(this, upIntent);
+//                }
+//                return true;
+//        }
+//        return false;
+//    }
 
     /**
      * This method stops fragment injection in malicious applications.
---
+++ List/app/src/test/java/com/yyktools/list/TableTest.java
@@ -0,0 +1,20 @@
+package com.yyktools.list;
+
+import android.widget.TableLayout;
+
+import org.json.JSONArray;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yyk on 12/26/2017.
+ */
+
+public class TableTest {
+    @Test
+    public void create_Table() throws Exception {
+        assertEquals(4, 2 + 2);
+
+    }
+}
